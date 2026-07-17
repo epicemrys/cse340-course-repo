a67fc06ce4fe414d2c7b2dc9f9f63f792c0a1ed4
Charles <cukoh@byupathway.edu>
2026-07-17 19:27:56 +0100

    Displaying Servie Projects

--- src/models/projects.js
+++ src/models/projects.js
@@ -21,6 +21,52 @@ const getAllProjects = async () => {
     return result.rows;
 };
 
+const getUpcomingProjects = async (numberOfProjects) => {
+    const query = `
+        SELECT
+            service_project.project_id,
+            service_project.title,
+            service_project.description,
+            service_project.project_date AS date,
+            service_project.location,
+            service_project.organization_id,
+            organization.name AS organization_name
+        FROM public.service_project
+        INNER JOIN public.organization
+            ON service_project.organization_id = organization.organization_id
+        WHERE service_project.project_date >= CURRENT_DATE
+        ORDER BY service_project.project_date ASC, service_project.title ASC
+        LIMIT $1;
+    `;
+
+    const queryParams = [numberOfProjects];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+};
+
+const getProjectDetails = async (id) => {
+    const query = `
+        SELECT
+            service_project.project_id,
+            service_project.title,
+            service_project.description,
+            service_project.project_date AS date,
+            service_project.location,
+            service_project.organization_id,
+            organization.name AS organization_name
+        FROM public.service_project
+        INNER JOIN public.organization
+            ON service_project.organization_id = organization.organization_id
+        WHERE service_project.project_id = $1;
+    `;
+
+    const queryParams = [id];
+    const result = await db.query(query, queryParams);
+
+    return result.rows.length > 0 ? result.rows[0] : null;
+};
+
 
 const getProjectsByOrganizationId = async (organizationId) => {
       const query = `
@@ -30,10 +76,10 @@ const getProjectsByOrganizationId = async (organizationId) => {
           title,
           description,
           location,
-          date
-        FROM project
+          project_date AS date
+        FROM service_project
         WHERE organization_id = $1
-        ORDER BY date;
+        ORDER BY project_date;
       `;
       
       const queryParams = [organizationId];
@@ -42,4 +88,9 @@ const getProjectsByOrganizationId = async (organizationId) => {
       return result.rows;
 };
 
-export { getAllProjects, getProjectsByOrganizationId };
+export {
+    getAllProjects,
+    getUpcomingProjects,
+    getProjectDetails,
+    getProjectsByOrganizationId
+};
